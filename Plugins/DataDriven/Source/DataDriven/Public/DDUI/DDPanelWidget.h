c9b8f9c31d7527b487be72ace775b607c75edea2
Administrator <Administrator@DESKTOP-O2ULPCN>
2021-12-08 17:27:06 +0800

    UI框架的数据结构和基本类的创建

---
+++ Plugins/DataDriven/Source/DataDriven/Public/DDUI/DDPanelWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DDObject/DDUserWidget.h"
+#include "DDPanelWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DATADRIVEN_API UDDPanelWidget : public UDDUserWidget
+{
+	GENERATED_BODY()
+	
+};
